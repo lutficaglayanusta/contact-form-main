bb26eb4ca70ee688c6debce6125b32791adc0b73
caglayan <caglayanustaa@gmail.com>
2025-11-28 20:23:46 +0300

    css add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,23 +1,70 @@
-import { Formik, Form, Field } from 'formik'
-import css from './ContactForm.module.css'
+import { Formik, Form, Field } from "formik";
+import css from "./ContactForm.module.css";
 
 const ContactForm = () => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+    picked: "",
+    checked: false,
+  };
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+
+    actions.resetForm();
+  };
+
   return (
     <div>
-      <Formik>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form className={css.form}>
-          <label htmlFor="name">First Name</label>
-          <Field id="name" name="name" placeholder="Your Name" />
+          <div>
+            <div>
+              <label htmlFor="firstName">First Name</label>
+              <Field id="firstName" name="firstName" placeholder="Your Name" />
+            </div>
 
-          <label htmlFor="name">Last Name</label>
-          <Field id="name" name="name" placeholder="Your Name" />
+            <div>
+              <label htmlFor="lastName">Last Name</label>
+              <Field
+                id="lastName"
+                name="lastName"
+                placeholder="Your Last Name"
+              />
+            </div>
+          </div>
 
           <label htmlFor="email">Email Address</label>
-          <Field id="email" name="email"></Field>
+          <Field id="email" name="email" placeholder="Email address"></Field>
+
+          <div>
+            <label>
+              <Field type="radio" name="picked" value="General Enquiry" />
+              General Enquiry
+            </label>
+
+            <label>
+              <Field type="radio" name="picked" value="Support Request" />
+              Support Request
+            </label>
+          </div>
+
+          <label htmlFor="message">Message</label>
+          <Field as="textarea" name="message" id="message" cols="20" rows="5" />
+
+          <label>
+            <Field type="checkbox" name="checked" value="true" />I consent to
+            being contacted by the team
+          </label>
+
+          <button type="submit">Submit</button>
         </Form>
       </Formik>
     </div>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
